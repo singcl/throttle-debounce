e3b349c576848614393e2c30b77bbf507058d399
singcl <24661881@qq.com>
2018-04-23 00:47:34 +0800

    :art: init throttle and debounce file.

---
+++ index.js
@@ -0,0 +1,11 @@
+var throttle = require('./throttle')
+var debounce = require('./debounce')
+
+/**
+ * throttle and debounce
+ * @module
+ */
+module.exports = {
+    throttle: throttle,
+    debounce: debounce
+}
