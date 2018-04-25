61a635b806fe00af8c0784f0889b75caa23a2edb
singcl <24661881@qq.com>
2018-04-25 23:55:33 +0800

    :art: test case construction.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,38 @@
-(function(window) {
+var qunit = require('qunitjs')
 
-    // unit test 1
-    test('ok test', function() {
-        ok(true, 'hello world');
-    })
+var throttle = require('../src/throttle')
 
-    // unit test 2
-    QUnit.test( "hello test", function( assert ) {
-        assert.ok( 1 === "1", "Passed!" );
-    });
-}(this))
+// Qunit.config.autostart = false
+
+var pause = 500
+var delay = 100
+
+function execManyTimes(each, complete) {
+    var i = 0
+    var repeated
+    var id
+
+    function start() {
+        id = setInterval(function() {
+            each()
+            if (++i === 50) {
+                clearInterval(id)
+                complete(repeated ? null : function() {
+                    i = 0
+                    repeated = true
+                    setTimeout(start, pause)
+                })
+            }
+        }, 20)
+    }
+
+    setTimeout(start, pause)
+}
+
+qunit.module('throttle')
+
+qunit.test('dealy callback', function() {
+    qunit.expect(7)
+    qunit.stop()
+})
+
