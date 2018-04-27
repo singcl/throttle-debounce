14da673b81afc68f14709ec22448b06a3c1f55e3
singcl <24661881@qq.com>
2018-04-28 00:59:13 +0800

    :white_check_mark: Karma reporter with mocha style logging.

--- karma.conf.js
+++ karma.conf.js
@@ -3,85 +3,101 @@
 
 var path = require('path')
 
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
-  
-      // base path that will be used to resolve all patterns (eg. files, exclude)
-      basePath: '',
-  
-  
-      // frameworks to use
-      // include browserify first in used frameworks.
-      // karma-browserify usage @see https://github.com/nikku/karma-browserify
-      // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-      frameworks: ['browserify', 'qunit'],
-  
-  
-      // list of files / patterns to load in the browser
-      files: [
-        'test/**/*.js'
-      ],
-  
-  
-      // list of files to exclude
-      exclude: [],
-  
-  
-      // preprocess matching files before serving them to the browser
-      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-      preprocessors: {
-        'test/**/*.js': ['browserify', 'coverage']
-      },
 
-      // optionally, configure the reporter
-      coverageReporter: {
-        dir: path.join(__dirname, 'coverage'),
-        reporters: [
-          {type: 'html'},
-          {type: 'lcov', subdir: 'lcov'}  // lcov
-        ]
-      },
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
-      // test results reporter to use
-      // possible values: 'dots', 'progress'
-      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-      // coverage reporter generates the coverage
-      reporters: ['progress', 'coverage'],
-  
-      // test results reporter to use
-      // possible values: 'dots', 'progress'
-      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-      //   reporters: ['mocha'],
 
-      // web server port
-      port: 9876,
-  
-  
-      // enable / disable colors in the output (reporters and logs)
-      colors: true,
-  
-  
-      // level of logging
-      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-      logLevel: config.LOG_INFO,
-  
-  
-      // enable / disable watching file and executing tests whenever any file changes
-      autoWatch: false,
-  
-  
-      // start these browsers
-      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-      browsers: ['Chrome', 'Firefox'],
-  
-  
-      // Continuous Integration mode
-      // if true, Karma captures browsers, runs the tests and exits
-      singleRun: false,
-  
-      // Concurrency level
-      // how many browser should be started simultaneous
-      concurrency: Infinity
+        // frameworks to use
+        // include browserify first in used frameworks.
+        // karma-browserify usage @see https://github.com/nikku/karma-browserify
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['browserify', 'qunit'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
+            'test/**/*.js'
+        ],
+
+
+        // list of files to exclude
+        exclude: [],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'test/**/*.js': ['browserify', 'coverage']
+        },
+
+        // optionally, configure the reporter
+        coverageReporter: {
+            dir: path.join(__dirname, 'coverage'),
+            reporters: [
+                { type: 'html' },
+                { type: 'lcov', subdir: 'lcov' }  // lcov
+            ]
+        },
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        // coverage reporter generates the coverage
+        reporters: ['coverage', 'mocha'],
+
+        // mocha reporter options
+        mochaReporter: {
+            colors: {
+                // success: 'blue',
+                // info: 'bgGreen',
+                // warning: 'cyan',
+                // error: 'bgRed'
+            },
+            symbols: {
+                // success: '+',
+                // info: '#',
+                // warning: '!',
+                // error: 'x'
+            }
+        },
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        //   reporters: ['mocha'],
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome', 'Firefox'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
     })
-  }
-  
+}
